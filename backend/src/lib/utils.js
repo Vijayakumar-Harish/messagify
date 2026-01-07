6bb18d6f778ad3744a9c6b5ba9855c1a7e932625
Vijayakumar-Harish <vijayakumarharisha@gmail.com>
2026-01-07 23:22:30 +0530

    signup section code refactoring

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -2,6 +2,10 @@ import jwt from "jsonwebtoken"
 
 
 export const generateToken = (userId, res) => {
+    const { JWT_SECRET, NODE_ENV } = process.env;
+    if (!JWT_SECRET) {
+        throw new Error("JWT_SECRET is not configured");
+    }
     const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
         expiresIn: "7d"
     })
@@ -10,7 +14,8 @@ export const generateToken = (userId, res) => {
         maxAge: 7 * 24 * 60 * 60 * 1000,
         httpOnly: true,
         sameSite: "strict",
-        secure: process.env.NODE_ENV === "development" ? false : true,
+        secure: NODE_ENV !== "development",
+        path:"/",
     });
 
     return token;
